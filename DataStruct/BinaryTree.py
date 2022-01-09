d32d2553cc2a7e8a5c568279580656eab3e67550
arron <fsxchen@gmail.com>
2022-01-09 19:08:22 +0800

    feat(binarytree): 二叉树遍历的非递归版本。

--- DataStruct/BinaryTree.py
+++ DataStruct/BinaryTree.py
@@ -1,3 +1,34 @@
+class StackFullException(Exception):
+  pass
+ 
+class StackEmptyException(Exception):
+  pass
+class Stack:
+    def __init__(self,size=100):
+        self.size = size # 给定存放数据的长度
+        self.lst = [] # 存放数据的列表
+        self.top = 0 # 栈顶指针
+ 
+    # 入栈
+    def push(self, el):
+        if self.top >= self.size:
+            raise StackFullException('your stack is full!!')
+        self.lst.insert(self.top, el) # 放元素
+        self.top += 1 # 栈顶指针向上移动一下 
+ 
+    # 出栈
+ 
+    def pop(self):
+        if self.top == 0:
+            raise StackEmptyException('your stack is empty!!!')
+        self.top -= 1
+        el = self.lst[self.top]
+        return el
+
+    def is_empty(self):
+        return self.top == 0
+    
+
 class Node:
     head = None
     left = None
@@ -30,3 +61,48 @@ class Node:
         self.pre(node.leftg)
         self.pre(node.right)
         print(node.value)
+
+    def pre_no_recurison(self, node):
+        if node != None:
+            stack = Stack()
+            stack.push(node)
+        while not stack.is_empty():
+            el = stack.pop()
+            print(el.value)
+
+            if el.right:
+                stack.push(el.head)
+            if el.left:
+                stack.push(el.left)
+
+    def pos_no_recurison(self, node):
+        if node != None:
+            stack = Stack()
+            stack.push(node)
+
+            s2 = Stack()
+        while not stack.is_empty():
+            el = stack.pop()
+            s2.push(el.value)
+
+            if el.left:
+                stack.push(el.left)
+            if el.right:
+                stack.push(el.head)
+        
+        while not s2.is_empty():
+            print(s2.pop())
+
+    
+    def mid_no_recurison(self, node):
+        if node:
+            stack = Stack()
+            while (not stack.is_empty()) or (not node):
+                if node:
+                    stack.push(node)
+                    node = node.left
+
+                else:
+                    node = stack.pop()
+                    print(node.value)
+                    node = node.right;
